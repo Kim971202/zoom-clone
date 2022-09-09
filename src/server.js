e4d94f65c90bb413dde45b7ce59a81fcb49c4916
Kim Donghyun <usccollege1202@gmail.com>
2022-09-09 20:51:06 +0900

    Frontend 설정 완료

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
-  import express from "express";
+import express from "express";
 
-  const app = express();
+const app = express();
+ 
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/home", (req,res) => {res.render("Home")});
 
- console.log('Hello');
-
-  app.listen(3000);
+ const handleListen = () =>  console.log(`Listening on http://localhost:3000`);
+ app.listen(3000, handleListen);
